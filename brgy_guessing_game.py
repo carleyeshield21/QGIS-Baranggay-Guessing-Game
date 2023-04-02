6d38a723035d3c2220749fe95b6c0f54140aab83
Carl Yamzon <92280593+carleyeshield21@users.noreply.github.com>
2023-04-02 16:10:03 +0800

    Update brgy_guessing_game.py

--- brgy_guessing_game.py
+++ brgy_guessing_game.py
@@ -1,5 +1,6 @@
 import turtle
 import pandas
+
 screen = turtle.Screen()
 
 screen = turtle.Screen()
@@ -14,13 +15,17 @@ district4_df = pandas.read_csv('final_brgy_list.csv')
 
 # print(district4_df)
 # print(district4_df.brgys_in_lowercase.to_list())
-# print(type(district4_df.brgys_in_lowercase.to_list()))
+# for brgy in district4_df.brgys_in_lowercase.to_list():
+#     if brgy == 'u.p. campus':
+#         print(brgy)
+# # print(type(district4_df.brgys_in_lowercase.to_list()))
 
 remaining_guess = 10
 t = turtle.Turtle()
 t1 = turtle.Turtle()
 t2 = turtle.Turtle()
 t3 = turtle.Turtle()
+t4 = turtle.Turtle()
 
 list_of_guesses = []
 correct_guesses = []
@@ -34,7 +39,6 @@ while remaining_guess != 0:
         idx = district4_df.brgys_in_lowercase.to_list().index(user_input)
         print(idx)
         print(district4_df.iloc[idx][2], district4_df.iloc[idx][3])
-        # t.hideturtle()
         t.penup()
         t.shape('circle')
         t.goto(district4_df.iloc[idx][2], district4_df.iloc[idx][3])
@@ -44,7 +48,8 @@ while remaining_guess != 0:
         t3.penup()
         t3.hideturtle()
         t3.goto(-250.0, 350.0)
-        t3.write(f'{len(correct_guesses)} out of 37 correct', font=('Arial', 20, 'bold'))
+        # t3.write(f'{len(correct_guesses)} out of 37 correct', font=('Arial', 20, 'bold'))
+        t3.write(f'{len(set(correct_guesses))} out of 37 correct', font=('Arial', 20, 'bold'))
 
     else:
         remaining_guess -= 1
@@ -53,9 +58,20 @@ while remaining_guess != 0:
         t2.penup()
         t2.goto(-273.0, 439.0)
         t2.write(f'remaining guesses = {remaining_guess}', font=('Arial', 20, 'bold'))
-        if remaining_guess == 0:
+
+        if len(list(set(correct_guesses))) == len(district4_df.brgys_in_lowercase.to_list()):
+            game_on = False
+            t4.hideturtle()
+            t4.penup()
+            t4.goto(0, 0)
+            t4.color('green')
+            t4.write('Congratulations!! You have named all the baranggays in the 4th District of QC',align='center',font=('Arial', 30, 'bold'))
+            break
+
+        elif remaining_guess == 0:
             game_on = False
             break
+
         else:
             # remaining_guess.update_guess()
             t1.hideturtle()
@@ -87,17 +103,19 @@ for brgy_missed in missed_brgys:
             district4_df.brgys_in_lowercase.to_list().index(brgy_missed)][3])
     t4.write(brgy_missed,align='center',font=('Arial', 15, 'normal'))
 
-t5 = turtle.Turtle()
-t5.hideturtle()
-t5.penup()
-t5.goto(150,-200)
-t5.write('These are the baranggays that you have missed', font=('Arial', 20, 'bold'))
+    if remaining_guess == 0:
+        t5 = turtle.Turtle()
+        t5.hideturtle()
+        t5.penup()
+        t5.goto(150, -200)
+        t5.write('These are the baranggays that you have missed', font=('Arial', 20, 'bold'))
 
-
-# t3.penup()
-# t3.hideturtle()
-# t3.goto(0,0)
-# t3.write(missed_brgys,align='center',font=('Arial', 20, 'bold'))
+# print(len(district4_df.brgys_in_lowercase.to_list()))
+# print(len(correct_guesses))
+# print(len(missed_brgys))
+print(set(correct_guesses))
+print(type(list(set(correct_guesses))))
+print(len(list(set(correct_guesses))))
 
 # screen.exitonclick()
 turtle.mainloop()
